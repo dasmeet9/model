5af890cd93bd8de32097196d274b71b5ef6be3d2
dasmeet9 <intern9@dasinfomedia.com>
2025-03-25 12:26:08 +0530

    Update meet3.js

--- meet3.js
+++ meet3.js
@@ -2,4 +2,4 @@ ChatbotWidget.init({
        containerId: 'chatbot-container', 
        cssUrl: 'https://dasmeet9.github.io/model/style.css'
 });
-BACKENDURL = 'http://127.0.0.1:5000';
+const BACKENDURL = 'http://127.0.0.1:5000';
