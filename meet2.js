f91bc9448073079e3edb8d853cc6bebdd43cbb91
dasmeet9 <intern9@dasinfomedia.com>
2025-03-28 14:46:57 +0530

    Update meet2.js

--- meet2.js
+++ meet2.js
@@ -1795,3 +1795,8 @@ function loadChatHistory() {
     };
   })();
   
+ChatbotWidget.init({
+       containerId: 'chatbot-container', 
+       cssUrl: 'https://dasmeet9.github.io/model/style.css'
+});
+const BACKENDURL = 'http://127.0.0.1:5000';
